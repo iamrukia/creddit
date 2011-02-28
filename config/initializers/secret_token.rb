0ed3c9cb423e00893a9b59e9ad43d2f42017c017
markvan <markvanharmelen@gmail.com>
2011-02-28 14:34:46 +0000

    initial setup of application after making rails and testing framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creddit::Application.config.secret_token = 'c0a4d7de6573d5f77c368e3f53419705af76f5625201233fd2344d434baaf7755f87d23ec8b4d0bada8e981804d65b8ff96714eade495e3ea7690d8034ff9eaa'
